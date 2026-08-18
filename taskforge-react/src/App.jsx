e27f372a9da0d9ff0ca9179386e99ceddc4b317c
SYED M. ZAIN BUKHARI <shahsyed1120@gmail.com>
2026-08-18 15:45:12 -0700

    Added Card Component
    added card component and passing values by props

--- taskforge-react/src/App.jsx
+++ taskforge-react/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Card from "./components/Card";
 function App() {
   return (
     <>
       <Navbar />
       <Footer />
+      <Card title="My Card 1" description="This is a simple card component." style={{backgroundColor: '#f0f0f0'}} />
+        <Card title="My Card 2" description="This is a simple card component." style={{backgroundColor: '#e0e0e0'}} />
+          <Card title="My Card 3" description="This is a simple card component." style={{backgroundColor: '#d0d0d0'}} />
     </>
   );
 }
